bd3a40eba8513b550a76e2a628ddea87f74621c6
Mark Paluch <mpaluch@pivotal.io>
2017-05-16 17:25:14 +0200

    Fallback to default port if Endpoint URI does not declare a port.
    We now use the default port (scheme-specific) if a URI does not declare an explicit port number.
    
    Fixes gh-99.

--- spring-vault-core/src/test/java/org/springframework/vault/client/VaultEndpointUnitTests.java
+++ spring-vault-core/src/test/java/org/springframework/vault/client/VaultEndpointUnitTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,13 +23,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Unit tests for {@link VaultEndpoint}.
- * 
+ *
  * @author Mark Paluch
  */
 public class VaultEndpointUnitTests {
 
 	@Test
-	public void shouldCreateEndpointFromHostAndPort() throws Exception {
+	public void shouldCreateEndpointFromHostAndPort() {
 
 		VaultEndpoint endpoint = VaultEndpoint.create("host", 1234);
 
@@ -39,12 +39,22 @@ public class VaultEndpointUnitTests {
 	}
 
 	@Test
-	public void shouldCreateEndpointFromURI() throws Exception {
+	public void shouldCreateEndpointFromURI() {
 
 		VaultEndpoint endpoint = VaultEndpoint.from(URI.create("http://127.0.0.1:443"));
 
 		assertThat(endpoint.getScheme()).isEqualTo("http");
 		assertThat(endpoint.getHost()).isEqualTo("127.0.0.1");
 		assertThat(endpoint.getPort()).isEqualTo(443);
+	}
+
+	@Test
+	public void shouldCreateEndpointFromURIWithoutPort() {
+
+		VaultEndpoint endpoint = VaultEndpoint.from(URI.create("http://127.0.0.1"));
+
+		assertThat(endpoint.getScheme()).isEqualTo("http");
+		assertThat(endpoint.getHost()).isEqualTo("127.0.0.1");
+		assertThat(endpoint.getPort()).isEqualTo(80);
 	}
 }
