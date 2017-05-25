f78992c8faebfa3317b997f8af67471e0f32ee2a
Mark Paluch <mpaluch@pivotal.io>
2017-05-25 17:43:57 +0200

    Polishing.
    Reformat code.
    
    See gh-94.

--- spring-vault-core/src/main/java/org/springframework/vault/authentication/TokenAuthentication.java
+++ spring-vault-core/src/main/java/org/springframework/vault/authentication/TokenAuthentication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,6 +37,7 @@ public class TokenAuthentication implements ClientAuthentication {
 	public TokenAuthentication(String token) {
 
 		Assert.hasText(token, "Token must not be empty");
+
 		this.token = VaultToken.of(token);
 	}
 
