7244973befc2463ac703484b3331363475e04a37
Mark Paluch <mpaluch@pivotal.io>
2017-11-23 12:07:44 +0100

    Make RoleId and SecretId public.
    AppRoleAuthenticationOptions.RoleId and AppRoleAuthenticationOptions.SecretId interfaces are now public.
    
    Closes gh-179.

--- spring-vault-core/src/main/java/org/springframework/vault/authentication/AppRoleAuthenticationOptions.java
+++ spring-vault-core/src/main/java/org/springframework/vault/authentication/AppRoleAuthenticationOptions.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -322,7 +322,7 @@ public class AppRoleAuthenticationOptions {
 	 *
 	 * @since 2.0
 	 */
-	interface RoleId {
+	public interface RoleId {
 
 		/**
 		 * Create a {@link RoleId} object that obtains its value from unwrapping a
@@ -376,7 +376,7 @@ public class AppRoleAuthenticationOptions {
 	 *
 	 * @since 2.0
 	 */
-	interface SecretId {
+	public interface SecretId {
 
 		/**
 		 * Create a {@link SecretId} object that obtains its value from unwrapping a
