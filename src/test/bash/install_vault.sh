972e77556df3950aff3ada38acf03c84dcbcfb95
Mark Paluch <mpaluch@pivotal.io>
2018-03-07 08:58:02 +0100

    Upgrade to Vault 0.9.5.
    Reduce build matrix by removal of intermediate Vault versions.
    
    Closes gh-212.

--- src/test/bash/install_vault.sh
+++ src/test/bash/install_vault.sh
@@ -6,7 +6,7 @@
 ###########################################################################
 
 
-VAULT_VER="${VAULT_VER:-0.9.3}"
+VAULT_VER="${VAULT_VER:-0.9.5}"
 UNAME=$(uname -s |  tr '[:upper:]' '[:lower:]')
 VAULT_ZIP="vault_${VAULT_VER}_${UNAME}_amd64.zip"
 IGNORE_CERTS="${IGNORE_CERTS:-no}"
