38819e88866ae07a3db9425fc0abf30f710fe115
DanielFran <dandrfranco@gmail.com>
2019-04-24 17:14:27 +0100

    Upgrade to Vault 1.1.2.
    see https://github.com/hashicorp/vault/blob/v1.1.2/CHANGELOG.md

--- src/test/bash/install_vault.sh
+++ src/test/bash/install_vault.sh
@@ -6,7 +6,7 @@
 ###########################################################################
 
 
-VAULT_VER="${VAULT_VER:-1.1.1}"
+VAULT_VER="${VAULT_VER:-1.1.2}"
 UNAME=$(uname -s |  tr '[:upper:]' '[:lower:]')
 VAULT_ZIP="vault_${VAULT_VER}_${UNAME}_amd64.zip"
 IGNORE_CERTS="${IGNORE_CERTS:-no}"
