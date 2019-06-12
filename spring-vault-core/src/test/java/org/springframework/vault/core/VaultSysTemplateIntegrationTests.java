fbe69bfb7cf6a099a228248d946dd62cf813fefd
Maciej Drozdzowski <maciej.drozdzowski@qantasloyalty.com>
2019-06-12 10:11:06 +1000

    Add options to VaultMount.
    Original pull request: gh-435.
    Closes gh-433.

--- spring-vault-core/src/test/java/org/springframework/vault/core/VaultSysTemplateIntegrationTests.java
+++ spring-vault-core/src/test/java/org/springframework/vault/core/VaultSysTemplateIntegrationTests.java
@@ -30,12 +30,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.vault.support.Policy;
 import org.springframework.vault.support.VaultMount;
+import org.springframework.vault.support.VaultResponse;
 import org.springframework.vault.support.VaultUnsealStatus;
 import org.springframework.vault.support.Policy.Rule;
 import org.springframework.vault.util.IntegrationTestSupport;
+import org.springframework.vault.util.VaultRule;
 import org.springframework.vault.util.Version;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.Assume.assumeTrue;
 import static org.springframework.vault.support.Policy.BuiltinCapabilities.READ;
 import static org.springframework.vault.support.Policy.BuiltinCapabilities.UPDATE;
@@ -82,8 +84,8 @@ public class VaultSysTemplateIntegrationTests extends IntegrationTestSupport {
 			adminOperations.unmount("other");
 		}
 
-		VaultMount mount = VaultMount.builder().type("generic") //
-				.config(Collections.singletonMap("default_lease_ttl", (Object) "1h")) //
+		VaultMount mount = VaultMount.builder().type("generic")
+				.config(Collections.singletonMap("default_lease_ttl", "1h"))
 				.description("hello, world").build();
 
 		adminOperations.mount("other", mount);
@@ -96,6 +98,76 @@ public class VaultSysTemplateIntegrationTests extends IntegrationTestSupport {
 		assertThat(secret.getDescription()).isEqualTo(mount.getDescription());
 		assertThat(secret.getConfig()).containsEntry("default_lease_ttl", 3600);
 		assertThat(Arrays.asList("kv", "generic")).contains(secret.getType());
+	}
+
+	@Test
+	public void mountShouldMountKv1Secret() {
+
+		assumeTrue(vaultVersion.isGreaterThanOrEqualTo(
+			VaultRule.VERSIONING_INTRODUCED_WITH));
+
+		if (adminOperations.getMounts().containsKey("kVv1/")) {
+			adminOperations.unmount("kVv1");
+		}
+
+		VaultMount mount = VaultMount.builder().type("kv")
+			.config(Collections.singletonMap("default_lease_ttl",  "1h"))
+			.description("hello, world").build();
+
+		adminOperations.mount("kVv1", mount);
+
+		Map<String, VaultMount> mounts = adminOperations.getMounts();
+
+		assertThat(mounts).containsKey("kVv1/");
+
+		VaultMount kVv1 = mounts.get("kVv1/");
+		assertThat(kVv1.getDescription()).isEqualTo(mount.getDescription());
+		assertThat(kVv1.getConfig()).containsEntry("default_lease_ttl", 3600);
+		assertThat(kVv1.getType()).isEqualTo("kv");
+
+		// a versioned write (kv put) will fail for a kv (default version: 1, not versioned) store
+		// make sure regular VaultTemplate.write/read operations work
+
+		vaultOperations.write("secret/mykey", Collections.singletonMap("hello", "world"));
+
+		VaultResponse read = vaultOperations.read("secret/mykey");
+		assertThat(read).isNotNull();
+		assertThat(read.getData()).containsEntry("hello", "world");
+	}
+
+	@Test
+	public void mountShouldMountKv2Secret() {
+
+		assumeTrue(vaultVersion.isGreaterThanOrEqualTo(
+			VaultRule.VERSIONING_INTRODUCED_WITH));
+
+		if (adminOperations.getMounts().containsKey("kVv2/")) {
+			adminOperations.unmount("kVv2");
+		}
+
+		VaultMount mount = VaultMount.builder().type("kv")
+			.config(Collections.singletonMap("default_lease_ttl", "1h"))
+			.options(Collections.singletonMap("version", "2"))
+			.description("hello, world").build();
+
+		adminOperations.mount("kVv2", mount);
+
+		Map<String, VaultMount> mounts = adminOperations.getMounts();
+
+		assertThat(mounts).containsKey("kVv2/");
+
+		VaultMount kVv2 = mounts.get("kVv2/");
+		assertThat(kVv2.getDescription()).isEqualTo(mount.getDescription());
+		assertThat(kVv2.getConfig()).containsEntry("default_lease_ttl", 3600);
+		assertThat(kVv2.getType()).isEqualTo("kv");
+		assertThat(kVv2.getOptions()).containsEntry("version", "2");
+
+		VaultVersionedKeyValueOperations versionedOperations =
+			vaultOperations.opsForVersionedKeyValue("kVv2");
+
+		versionedOperations.put("secret/mykey", Collections.singletonMap("key", "value"));
+		assertThat(versionedOperations.get("secret/mykey").getData())
+			.containsEntry("key", "value");
 	}
 
 	@Test
