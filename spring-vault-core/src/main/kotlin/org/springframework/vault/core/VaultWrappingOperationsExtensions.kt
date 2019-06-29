bcb6090a012c6610d750058090d0cd04f08e7afd
Mark Paluch <mpaluch@pivotal.io>
2019-06-30 00:02:54 +0200

    Add Kotlin extensions.
    We now provide extensions for VaultOperations, ReactiveVaultOperations, VaultKeyValueOperations, VaultVersionedKeyValueOperations, and VaultWrappingOperations to leverage reified type parameters with read(…) methods.
    
    Closes gh-442.

---
+++ spring-vault-core/src/main/kotlin/org/springframework/vault/core/VaultWrappingOperationsExtensions.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.vault.core
+
+import org.springframework.vault.support.VaultResponseSupport
+import org.springframework.vault.support.VaultToken
+
+/**
+ * Extension for [VaultWrappingOperations.get] leveraging reified type parameters.
+ *
+ * @author Mark Paluch
+ * @since 2.2
+ */
+@Suppress("EXTENSION_SHADOWED_BY_MEMBER")
+inline fun <reified T : Any> VaultWrappingOperations.read(token: VaultToken): VaultResponseSupport<T>? =
+        read(token, T::class.java)
---
+++ spring-vault-core/src/test/kotlin/org/springframework/vault/core/ReactiveVaultOperationsExtensionsTests.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.vault.core
+
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
+
+/**
+ * Unit tests for [ReactiveVaultOperationsExtensions].
+ *
+ * @author Mark Paluch
+ */
+class ReactiveVaultOperationsExtensionsTests {
+
+    val operation = mockk<ReactiveVaultOperations>(relaxed = true)
+
+    @Test
+    fun `ReactiveVaultOperations#read() with reified type parameter extension should call its Java counterpart`() {
+
+        operation.read<Person>("the-path")
+        verify { operation.read("the-path", Person::class.java) }
+    }
+
+    @Test
+    fun `ReactiveVaultOperations#read() should call its Java counterpart`() {
+
+        operation.read("the-path")
+        verify { operation.read("the-path") }
+    }
+
+    class Person
+}
