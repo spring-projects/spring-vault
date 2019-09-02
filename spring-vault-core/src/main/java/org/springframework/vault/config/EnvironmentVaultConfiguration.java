0acc999020b9865e82ff7f5067ddc12a34269c16
hitenny <hitenny@gmail.com>
2019-09-02 20:02:14 +0530

    Set default kubernetes path if not configured

--- spring-vault-core/src/main/java/org/springframework/vault/config/EnvironmentVaultConfiguration.java
+++ spring-vault-core/src/main/java/org/springframework/vault/config/EnvironmentVaultConfiguration.java
@@ -367,6 +367,9 @@ public class EnvironmentVaultConfiguration extends AbstractVaultConfiguration
 				tokenFile);
 				
 		String path = getEnvironment().getProperty("vault.kubernetes.kubernetes-path");
+		if (!StringUtils.hasText(path)) {
+			path = KubernetesAuthenticationOptions.DEFAULT_KUBERNETES_AUTHENTICATION_PATH;
+		}
 		KubernetesAuthenticationOptions authenticationOptions = KubernetesAuthenticationOptions
 				.builder() //
 				.role(role) //
