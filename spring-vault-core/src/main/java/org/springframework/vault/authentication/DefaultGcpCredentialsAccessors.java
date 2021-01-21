6bfd192dd82da1c52eb13b62854b2a1e69edebcc
Andreas Gebauer <andreas.gebauer.berlin@gmail.com>
2021-01-21 16:59:11 +0100

    Add support for GCP IAM credentials API.
    We now support the IAM Credentials API in addition to the deprecated IAM API for signing JWT.
    
    Closes gh-600.
    Original pull request: gh-619.

---
+++ spring-vault-core/src/main/java/org/springframework/vault/authentication/DefaultGcpCredentialsAccessors.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.vault.authentication;
+
+import org.springframework.util.Assert;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+
+/**
+ * Default implementation of {@link GcpCredentialsAccountIdAccessor}. Used by
+ * {@link GcpIamCredentialsAuthentication}.
+ *
+ * @author Andreas Gebauer
+ * @since 2.4
+ * @see GcpIamCredentialsAuthentication
+ */
+enum DefaultGcpCredentialsAccessors implements GcpCredentialsAccountIdAccessor {
+
+	INSTANCE;
+
+	/**
+	 * Get a the service account id (email) to be placed in the signed JWT.
+	 * @param credentials credentials object to obtain the service account id from.
+	 * @return the service account id to use.
+	 */
+	@Override
+	public String getServiceAccountId(GoogleCredentials credentials) {
+
+		Assert.notNull(credentials, "GoogleCredentials must not be null");
+		Assert.isInstanceOf(ServiceAccountCredentials.class, credentials,
+				"The configured GoogleCredentials does not represent a service account. Configure the service account id with GcpIamCredentialsAuthenticationOptionsBuilder#serviceAccountId(String).");
+
+		return ((ServiceAccountCredentials) credentials).getAccount();
+	}
+
+}
