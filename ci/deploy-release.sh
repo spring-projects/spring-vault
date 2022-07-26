8ed8ba99a267cf1d7daa8d0f598d319e7155c751
Mark Paluch <mpaluch@vmware.com>
2022-07-26 15:54:37 +0200

    Ship releases using Jenkins
    See gh-719

---
+++ ci/deploy-release.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -euo pipefail
+
+GNUPGHOME=/tmp/gpghome
+export GNUPGHOME
+
+mkdir $GNUPGHOME
+cp $KEYRING $GNUPGHOME
+
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw -s settings.xml -Pci,central,release \
+      -Dgpg.passphrase=${PASSPHRASE} \
+      -Dgpg.secretKeyring=${GNUPGHOME}/secring.gpg \
+      -DstagingDescription="Releasing Spring Vault ${VERSION}" \
+      clean deploy -U -B
+
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" ./mvnw -s settings.xml -Pdistribute \
+      -Dartifactory.server=https://repo.spring.io \
+      -Dartifactory.username=${ARTIFACTORY_USR} \
+      -Dartifactory.password=${ARTIFACTORY_PSW} \
+      -Dartifactory.staging-repository=temp-private-local \
+      clean deploy -U -B
