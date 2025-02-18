1d4acb4a5b7a55b073775e53b594e90d36148e0d
Mark Paluch <mark.paluch@broadcom.com>
2025-02-18 15:03:52 +0100

    Upgrade to ValueExpression for secret backend expressions.
    See #898

--- spring-vault-core/src/main/java/org/springframework/vault/repository/mapping/BasicVaultPersistentEntity.java
+++ spring-vault-core/src/main/java/org/springframework/vault/repository/mapping/BasicVaultPersistentEntity.java
@@ -15,15 +15,16 @@
  */
 package org.springframework.vault.repository.mapping;
 
+import org.springframework.data.expression.ValueExpression;
+import org.springframework.data.expression.ValueExpressionParser;
 import org.springframework.data.keyvalue.core.mapping.AnnotationBasedKeySpaceResolver;
 import org.springframework.data.keyvalue.core.mapping.BasicKeyValuePersistentEntity;
 import org.springframework.data.keyvalue.core.mapping.KeySpaceResolver;
 import org.springframework.data.util.TypeInformation;
 import org.springframework.expression.Expression;
-import org.springframework.expression.ParserContext;
 import org.springframework.expression.common.LiteralExpression;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.lang.Nullable;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 /**
@@ -35,11 +36,11 @@ import org.springframework.util.StringUtils;
 public class BasicVaultPersistentEntity<T> extends BasicKeyValuePersistentEntity<T, VaultPersistentProperty>
 		implements VaultPersistentEntity<T> {
 
-	private static final SpelExpressionParser PARSER = new SpelExpressionParser();
+	private static final ValueExpressionParser PARSER = ValueExpressionParser.create();
 
 	private final String backend;
 
-	private final @Nullable Expression backendExpression;
+	private final @Nullable ValueExpression backendExpression;
 
 	/**
 	 * Creates new {@link BasicVaultPersistentEntity}.
@@ -86,10 +87,9 @@ public class BasicVaultPersistentEntity<T> extends BasicKeyValuePersistentEntity
 	 * @return
 	 */
 	@Nullable
-	private static Expression detectExpression(String potentialExpression) {
-
-		Expression expression = PARSER.parseExpression(potentialExpression, ParserContext.TEMPLATE_EXPRESSION);
-		return expression instanceof LiteralExpression ? null : expression;
+	private static ValueExpression detectExpression(String potentialExpression) {
+		ValueExpression expression = PARSER.parse(potentialExpression);
+		return expression.isLiteral() ? null : expression;
 	}
 
 	@Override
@@ -102,7 +102,7 @@ public class BasicVaultPersistentEntity<T> extends BasicKeyValuePersistentEntity
 
 		return this.backendExpression == null //
 				? this.backend //
-				: this.backendExpression.getValue(getEvaluationContext(null), String.class);
+				: ObjectUtils.nullSafeToString(this.backendExpression.evaluate(getValueEvaluationContext(null)));
 	}
 
 }
