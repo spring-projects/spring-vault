ec1d9673546b0c08b25768017fb790048c43987b
Mark Paluch <mark.paluch@broadcom.com>
2025-06-16 10:08:26 +0200

    Add missing variables.
    See #917

--- ci/deploy-milestone.sh
+++ ci/deploy-milestone.sh
@@ -2,6 +2,12 @@
 
 set -euo pipefail
 
+GNUPGHOME=/tmp/gpghome
+export GNUPGHOME
+
+mkdir $GNUPGHOME
+cp $KEYRING $GNUPGHOME
+
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-vault" ./mvnw -s settings.xml -Pci,central,release \
       -Dgpg.passphrase=${PASSPHRASE} \
       -Dgpg.secretKeyring=${GNUPGHOME}/secring.gpg \
