adeb59ab5683ffc0755383b6e19786e480658899
Mark Paluch <mark.paluch@broadcom.com>
2025-06-16 10:08:26 +0200

    Add missing variables.
    See #917

--- ci/deploy-milestone.sh
+++ ci/deploy-milestone.sh
@@ -2,9 +2,16 @@
 
 set -euo pipefail
 
+GNUPGHOME=/tmp/gpghome
+export GNUPGHOME
+
+mkdir $GNUPGHOME
+cp $KEYRING $GNUPGHOME
+chmod -R go-rwx $GNUPGHOME
+
+export MAVEN_GPG_PASSPHRASE="${PASSPHRASE}"
+
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-vault" ./mvnw -s settings.xml -Pci,central,release \
-      -Dgpg.passphrase=${PASSPHRASE} \
-      -Dgpg.secretKeyring=${GNUPGHOME}/secring.gpg \
       clean deploy -U -B
 
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-vault" ./mvnw -s settings.xml -Pdistribute \
--- ci/deploy-release.sh
+++ ci/deploy-release.sh
@@ -7,10 +7,11 @@ export GNUPGHOME
 
 mkdir $GNUPGHOME
 cp $KEYRING $GNUPGHOME
+chmod -R go-rwx $GNUPGHOME
+
+export MAVEN_GPG_PASSPHRASE="${PASSPHRASE}"
 
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-vault" ./mvnw -s settings.xml -Pci,central,release \
-      -Dgpg.passphrase=${PASSPHRASE} \
-      -Dgpg.secretKeyring=${GNUPGHOME}/secring.gpg \
       clean deploy -U -B
 
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-vault" ./mvnw -s settings.xml -Pdistribute \
