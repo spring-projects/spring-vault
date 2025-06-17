cdcb87bb51553a7b909ed5048462bd43a539149a
Mark Paluch <mark.paluch@broadcom.com>
2025-06-17 11:24:14 +0200

    Document relative and absolute Vault path behavior.
    Closes gh-926

--- spring-vault-core/src/main/java/org/springframework/vault/core/VaultOperations.java
+++ spring-vault-core/src/main/java/org/springframework/vault/core/VaultOperations.java
@@ -20,6 +20,7 @@ import java.util.List;
 import org.jspecify.annotations.Nullable;
 
 import org.springframework.vault.VaultException;
+import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.core.VaultKeyValueOperationsSupport.KeyValueBackend;
 import org.springframework.vault.support.VaultResponse;
 import org.springframework.vault.support.VaultResponseSupport;
@@ -33,6 +34,11 @@ import org.springframework.web.client.RestClientException;
  * {@link VaultOperations} allows execution of callback methods. Callbacks can execute
  * requests within a {@link #doWithSession(RestOperationsCallback) session context} and
  * the {@link #doWithVault(RestOperationsCallback) without a session}.
+ * <p>
+ * Paths used in this interface (and interfaces accessible from here) are considered
+ * relative to the {@link VaultEndpoint}. Paths that are fully-qualified URI's can be used
+ * to access Vault cluster members in an authenticated context. To prevent unwanted full
+ * URI access, make sure to sanitize paths before passing them to this interface.
  *
  * @author Mark Paluch
  * @author Lauren Voswinkel
