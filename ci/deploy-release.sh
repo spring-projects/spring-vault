9a05ecd2172ef56139e4c28ac5d745848ddf05b1
Mark Paluch <mark.paluch@broadcom.com>
2025-11-03 10:16:39 +0100

    Use GitHub actions for CI.
    Closes gh-948

--- ci/deploy-release.sh
+++ ci/deploy-release.sh
@@ -11,12 +11,4 @@ chmod -R go-rwx $GNUPGHOME
 
 export MAVEN_GPG_PASSPHRASE="${PASSPHRASE}"
 
-MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-vault" ./mvnw -s settings.xml -Pci,central,release \
-      clean deploy -U -B
-
-MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-vault" ./mvnw -s settings.xml -Pdistribute \
-      -Dartifactory.server=https://repo.spring.io \
-      -Dartifactory.username=${ARTIFACTORY_USR} \
-      -Dartifactory.password=${ARTIFACTORY_PSW} \
-      -Dartifactory.staging-repository=temp-private-local \
-      clean deploy -U -B
+MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home -Dmaven.repo.local=/tmp/jenkins-home/.m2/spring-vault" ./ci/deploy-maven-central.sh
