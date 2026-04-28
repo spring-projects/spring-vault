90266e40bbcac73157bcc3bd7d80eda62d6a3304
Mark Paluch <mark.paluch@broadcom.com>
2026-04-28 15:47:16 +0200

    Refine Vault installation and download.
    See #995

--- src/test/bash/env.sh
+++ src/test/bash/env.sh
@@ -9,4 +9,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export VAULT_TOKEN=00000000-0000-0000-0000-000000000000
 export VAULT_ADDR=https://localhost:8200
 export VAULT_SKIP_VERIFY=false
-export VAULT_CAPATH=${DIR}/work/ca/certs/ca.cert.pem
+export VAULT_CAPATH="${DIR}/work/ca/certs/ca.cert.pem"
